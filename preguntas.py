a619978ff66039ea0a4fed8af82ef1482d8d8fde
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-12-02 17:11:10 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -46,11 +46,12 @@ def pregunta_02():
     Complete el código presentado a continuación.
 
     """
-    # Lea el archivo de datos
+   # Lea el archivo de datos
     df = pd.read_csv("house-votes-84.csv", sep=",")
 
     # Cree un vector con la variable de respuesta ('party')
     y = df["party"].values
+
     # Extraiga las variables de entrada
     X = df.drop(["party"], axis=1).values
 
@@ -62,7 +63,6 @@ def pregunta_02():
 
     # Importe KNeighborsClassifier de sklearn.neighbors
     from sklearn.neighbors import KNeighborsClassifier
-    
 
     # Cree un un clasificador k-NN con 6 vecinos
     knn = KNeighborsClassifier(n_neighbors=6)
